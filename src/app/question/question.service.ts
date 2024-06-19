3f5f8424d3c11785b7b5e389421e0455b1e10ce9
cboissiere <clemboissiere@gmail.com>
2024-06-19 21:45:43 +0200

    update l'adresse

--- src/app/question/question.service.ts
+++ src/app/question/question.service.ts
@@ -3,13 +3,14 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LocalStorageService } from '../common/local-storage.service';
 import { QuestionResponse } from './question.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  private apiUrl = "http://localhost:8080/test";
+  private apiUrl = environment.apiUrl + "/test";
   private http: HttpClient = inject(HttpClient);
   private localStorageService = inject(LocalStorageService);
 
